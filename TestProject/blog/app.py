e2b5635adca6f38c2a011832a2ff3ecbb594f383
kemechial <kaanevran@gmail.com>
2024-10-17 20:43:15 +0300

    More test cases.

--- TestProject/blog/app.py
+++ TestProject/blog/app.py
@@ -1,10 +1,25 @@
 blogs = dict()
-
+MENU_PROMPT = ("Please enter a key to continue. (q, quit, c, create blog \
+                , l, print, r, read, p, create post)")
 
 def menu():
     print_blogs()
+    selection = input(MENU_PROMPT)
+    while selection != 'q':
+        if selection == 'c':
+            ask_create_blog()
+        elif selection == 'l':
+            print_blogs()
+        elif selection == 'r':
+            read_blog()
 
+def ask_create_blog():
+    pass
+
+def read_blog():
+    pass
 
 def print_blogs():
     for name,blog in blogs.items():
         print("name: {} - blog: {}".format(name,blog))
+
--- TestProject/blog/tests/system/test_system.py
+++ TestProject/blog/tests/system/test_system.py
@@ -6,6 +6,17 @@ from blog.blog import Blog
 
 
 class AppTest(TestCase):
+    def test_menu_input(self):
+        with patch('builtins.input') as mocked_input:
+            app.menu()
+            mocked_input.assert_called_with(app.MENU_PROMPT)
+
+    def test_call_print_blogs(self):
+        with patch("blog.app.print_blogs") as mocked_print_blogs:
+            with patch('builtins.input', return_value = 'q'):
+              app.menu()
+              mocked_print_blogs.assert_called()
+
     def test_print_blogs(self):
         blog = Blog('test', 'author')
         app.blogs = {'test': blog}
